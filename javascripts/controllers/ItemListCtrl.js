1a0aba90209320a271912ea303d4fffe6e956dba
Andrea Finch <aefinch@vt.edu>
2017-05-15 21:33:09 -0500

    created factory

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -1,28 +1,10 @@
-app.controller("ItemListCtrl", function($http, $q, $scope, FIREBASE_CONFIG){
+app.controller("ItemListCtrl", function($scope, ItemFactory){
 	$scope.items = [];
-	let getItemList = () => {
-		let itemz = [];                   //$q represents "new Promise"
-		return $q((resolve, reject) => {
-			$http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)        //use $http instead of $.ajax
-			.then((fbItems) => {
-				var itemCollection = fbItems.data;
-	            Object.keys(itemCollection).forEach((key) => {
-		            itemCollection[key].id=key;
-		            itemz.push(itemCollection[key]);
-
-          });
-          resolve(itemz);
-			}).catch((error) => {
-				reject(error);
-			});
-		});
-	};
-	let getItems = () => {
-		getItemList().then((itemz) => {
-			$scope.items = itemz;
-		}).catch((error) => {
-			console.log("get Error", error);
-		});
-	};
-	getItems();
+		
+	ItemFactory.getItemList().then((itemz) => {
+		$scope.items = itemz;
+	}).catch((error) => {
+		console.log("get Error", error);
+	});
+	
 });
---
+++ javascripts/factories/ItemFactory.js
@@ -0,0 +1,31 @@
+app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG){
+	let getItemList = () => {
+		let itemz = [];                   //$q represents "new Promise"
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)        //use $http instead of $.ajax
+			.then((fbItems) => {
+				var itemCollection = fbItems.data;
+	            Object.keys(itemCollection).forEach((key) => {
+		            itemCollection[key].id=key;
+		            itemz.push(itemCollection[key]);
+
+          });
+          resolve(itemz);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	};
+	let postNewItem = (newItem) => {
+		return $q((resolve, reject) => {
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
+			.then((resultz) => {
+				resolve(resultz);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	};
+
+	return {getItemList:getItemList, postNewItem:postNewItem};
+});
