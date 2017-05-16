91001371c22f88da3bfdde1b40101e4124ba18f4
Andrea Finch <aefinch@vt.edu>
2017-05-16 18:49:48 -0500

    add task is working

--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,11 +1,12 @@
 
-app.controller("ItemNewCtrl", function($scope, ItemFactory){
+app.controller("ItemNewCtrl", function($location, $scope, ItemFactory){
 
 	$scope.addNewItem = () => {
 		$scope.newTask.isCompleted = false;
 		ItemFactory.postNewItem($scope.newTask).then(() => {
 			$scope.newTask = {};
 			//switch views
+			$location.url("/items/list");
 		}).catch((error) => {
 			console.log("Add error", error);
 		});
